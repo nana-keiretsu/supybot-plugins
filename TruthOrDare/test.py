d54684742698984b8eafd2dd7e9034902091541b
SevensSystem <sevenssystem@gmail.com>
2014-06-11 13:52:45 -0400

    Nix Line Endings Based test.py

--- TruthOrDare/test.py
+++ TruthOrDare/test.py
@@ -3,3 +3,8 @@
 # Full license text can be found here: http://www.wtfpl.net/about/
 # I'd put copyright, but that's inherent in the US and I prefer copyleft.
 ###
+
+from supybot.test import *
+
+class TruthOrDareTestCase(PluginTestCase):
+    plugins = ('TruthOrDare',)
