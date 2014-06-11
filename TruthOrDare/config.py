777f0a79cf94d6be4497541f964308ceeeb8c9fa
SevensSystem <sevenssystem@gmail.com>
2014-06-11 13:49:23 -0400

    Initial config.py

--- TruthOrDare/config.py
+++ TruthOrDare/config.py
@@ -3,3 +3,12 @@
 # Full license text can be found here: http://www.wtfpl.net/about/
 # I'd put copyright, but that's inherent in the US and I prefer copyleft.
 ###
+
+import supybot.conf as conf
+import supybot.registry as registry
+
+def configure(advanced):
+    from supybot.questions import expect, anything, something, yn
+    conf.registerPlugin('TruthOrDare', True)
+
+TruthOrDare = conf.registerPlugin('TruthOrDare')
